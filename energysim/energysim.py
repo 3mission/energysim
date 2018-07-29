cea1ac3c8b531f1ca1900c063ff7728ec9c03243
EnergyGuy3 <39009854+EnergyGuy3@users.noreply.github.com>
2018-07-29 18:30:25 +0000

    Update energysim.py

--- energysim/energysim.py
+++ energysim/energysim.py
@@ -129,7 +129,7 @@ class energy():
     def _infra_totals(self):
 
         # compute total values
-        smartphone_total = self.infra_result[7] * self.infra_result[8] / 1000
+        smartphone_total = 'Smartphone.usage' * self.infra_result[7] * self.infra_result[8] / 1000
         pc_total = self.infra_result[9] * self.infra_result[10] / 1000
         laptop_total = self.infra_result[11] * self.infra_result[12] / 1000
         tablet_total = self.infra_result[13] * self.infra_result[14] / 1000
